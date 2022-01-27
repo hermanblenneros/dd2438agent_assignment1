ad9cbcb6f56aa8a81d870683cd61221b682a86fa
superegoo <595434909@qq.com>
2022-01-27 10:22:03 +0100

    basic update

--- Scrips/CarAI.cs
+++ Scrips/CarAI.cs
@@ -13,6 +13,25 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public GameObject terrain_manager_game_object;
         TerrainManager terrain_manager;
+        //new items
+        List<Vector3> OpenList = new List<Vector3>();
+        List<Vector3> CloseList = new List<Vector3>();
+        //
+        //new fuction
+        public bool leastcost(Vector3 node)//need to code
+        {
+            if (true)
+            {
+                return true;
+            }
+            return false;
+        }
+        private Vector3 RoundState(Vector3 point)
+        {
+
+            return null;
+        }
+        //new fuction end
 
         private void Start()
         {
@@ -31,6 +50,64 @@ namespace UnityStandardAssets.Vehicles.Car
 
             my_path.Add(start_pos);
 
+            //hybrid A* 
+            List<Vector3> result = new List<Vector3>();
+            OpenList.Add(start_pos);
+            Vector3 pred = new Vector3();
+            Vector3 succ = new Vector3();
+
+            while (OpenList!=null)
+            {
+                Vector3 pred = OpenList.Find(leastcost);
+                OpenList.Remove(pred);
+                if(CloseList.Contains(pred))
+                    continue;
+                else
+                    CloseList.Add(pred);
+                succ = RoundState(pred);
+                if (succ == RoundState(goal_pos))
+                {
+                    my_path.Add(succ);
+                    break;
+                }
+                else
+                {
+                    //Search with dubin or reed-shepp
+                    if (true)
+                    {
+                        succ = Dubin()
+                    }
+                    for(int i = 0; i < 6; i++)
+                    {
+                        Vector3 succ = pred + new Vector3(UnityEngine.Random.Range(-50.0f, 50.0f), 0, UnityEngine.Random.Range(-30.0f, 30.0f));
+                        //ensure successor is on grid and traversable
+                        if (succ)
+                        {
+                            if (!CloseList.Contains(succ))
+                            {
+                                succ = Update();
+
+                                //if the successor is in the same cell but the C value is larger
+                                if (true)
+                                { }
+                                //if successor is in the same cell and the C value is lower, set predecessor to predecessor of predecessor
+                                else if (false) 
+                                { }
+
+                                OpenList.Add(succ);
+
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+
+
+
+            //
             for (int i = 0; i < 3; i++)
             {
                 Vector3 waypoint = start_pos + new Vector3(UnityEngine.Random.Range(-50.0f, 50.0f), 0, UnityEngine.Random.Range(-30.0f, 30.0f));
@@ -80,5 +157,7 @@ namespace UnityStandardAssets.Vehicles.Car
             m_Car.Move(1f, 1f, 1f, 0f);
 
         }
+
+
     }
 }
