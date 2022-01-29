ce6670fa535968c7fdf2133f5477fdf7a1cb85aa
superegoo <595434909@qq.com>
2022-01-30 00:56:59 +0100

    Node applied

--- Scrips/CarAI.cs
+++ Scrips/CarAI.cs
@@ -13,25 +13,6 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public GameObject terrain_manager_game_object;
         TerrainManager terrain_manager;
-        //new items
-        List<Vector3> OpenList = new List<Vector3>();
-        List<Vector3> CloseList = new List<Vector3>();
-        //
-        //new fuction
-        public bool leastcost(Vector3 node)//need to code
-        {
-            if (true)
-            {
-                return true;
-            }
-            return false;
-        }
-        private Vector3 RoundState(Vector3 point)
-        {
-
-            return null;
-        }
-        //new fuction end
 
         private void Start()
         {
@@ -46,83 +27,21 @@ namespace UnityStandardAssets.Vehicles.Car
             Vector3 start_pos = terrain_manager.myInfo.start_pos;
             Vector3 goal_pos = terrain_manager.myInfo.goal_pos;
 
-            List<Vector3> my_path = new List<Vector3>();
 
-            my_path.Add(start_pos);
+            List<Node> my_path = new List<Node>();
 
-            //hybrid A* 
-            List<Vector3> result = new List<Vector3>();
-            OpenList.Add(start_pos);
-            Vector3 pred = new Vector3();
-            Vector3 succ = new Vector3();
-
-            while (OpenList!=null)
-            {
-                Vector3 pred = OpenList.Find(leastcost);
-                OpenList.Remove(pred);
-                if(CloseList.Contains(pred))
-                    continue;
-                else
-                    CloseList.Add(pred);
-                succ = RoundState(pred);
-                if (succ == RoundState(goal_pos))
-                {
-                    my_path.Add(succ);
-                    break;
-                }
-                else
-                {
-                    //Search with dubin or reed-shepp
-                    if (true)
-                    {
-                        succ = Dubin()
-                    }
-                    for(int i = 0; i < 6; i++)
-                    {
-                        Vector3 succ = pred + new Vector3(UnityEngine.Random.Range(-50.0f, 50.0f), 0, UnityEngine.Random.Range(-30.0f, 30.0f));
-                        //ensure successor is on grid and traversable
-                        if (succ)
-                        {
-                            if (!CloseList.Contains(succ))
-                            {
-                                succ = Update();
-
-                                //if the successor is in the same cell but the C value is larger
-                                if (true)
-                                { }
-                                //if successor is in the same cell and the C value is lower, set predecessor to predecessor of predecessor
-                                else if (false) 
-                                { }
-
-                                OpenList.Add(succ);
-
-                            }
-
-                        }
-                    }
-
-                }
-
-            }
-
-
-
-            //
-            for (int i = 0; i < 3; i++)
-            {
-                Vector3 waypoint = start_pos + new Vector3(UnityEngine.Random.Range(-50.0f, 50.0f), 0, UnityEngine.Random.Range(-30.0f, 30.0f));
-                my_path.Add(waypoint);
-            }
-            my_path.Add(goal_pos);
-
+            HybridAstar has = new HybridAstar(terrain_manager, m_Car);
+            my_path = has.Build();
+            Node goal = new Node(null, goal_pos, 0, 0, Mathf.PI / 2);
+            my_path.Add(goal);
 
             // Plot your path to see if it makes sense
             // Note that path can only be seen in "Scene" window, not "Game" window
             Vector3 old_wp = start_pos;
             foreach (var wp in my_path)
             {
-                Debug.DrawLine(old_wp, wp, Color.red, 100f);
-                old_wp = wp;
+                Debug.DrawLine(old_wp, wp.pos, Color.red, 100f);
+                old_wp = wp.pos;
             }
 
             
